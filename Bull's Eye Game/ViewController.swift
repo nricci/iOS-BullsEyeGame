11649f193dbe632eb22c2df93eda36d8af5baf72
Nicolas Ricci <nricci.02@gmail.com>
2017-06-15 08:19:57 -0300

    Initial Commit

---
+++ Bull's Eye Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bull's Eye Game
+//
+//  Created by Nicolas Ricci on 15/6/17.
+//  Copyright © 2017 Nicolas Ricci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
